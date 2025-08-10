9a13969518544a326992d80752d167c8032784bb
Gumaro Monroy Vazquez <maromvz@gmail.com>
2025-08-09 22:47:38 -0600

    refactor: update ESLint configuration and improve import statements in index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,19 @@
-const functions = require('firebase-functions');
-const express = require('express');
-import {Request, Response} from 'express';
-import {ngExpressEngine} from '@nguniversal/express-engine'
-const { join } = require('path');
-const { AppServerModule } = require('./dist/server/main');
+import functions from 'firebase-functions';
+import express from 'express';
+import { Request, Response } from "express";
+import { ngExpressEngine } from "@nguniversal/express-engine";
+import { join } from 'path';
+const { AppServerModule } = require("./dist/server/main");
 
-// Express server
 const app = express();
-// Set the view engine
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModule,
+app.engine("html", ngExpressEngine({
+    bootstrap: AppServerModule,
 }));
-app.set('view engine', 'html');
-app.set('views', join(__dirname, 'dist', 'browser'));// Serve static files
-app.get('*.*', express.static(join(__dirname, 'dist', 'browser')));// All regular routes use the Universal engine
-app.get('*', (req: Request, res: Response) => {
-  res.render('index', { req });
+app.set("view engine", "html");
+app.set("views", join(__dirname, "dist", "browser"));
+app.get("*.*", express.static(join(__dirname, "dist", "browser")));
+app.get("*", (req: Request, res: Response) => {
+    res.render("index", { req });
 });
 
-// Firebase Function
 exports.ssr = functions.https.onRequest(app);
